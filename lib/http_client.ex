374b38e827d356d602203fe62a1ea22175cc7d0f
László Horváth <bedafoltyn@gmail.com>
2019-01-18 18:44:46 +0100

    bad certificate bug fixed

--- lib/http_client.ex
+++ lib/http_client.ex
@@ -3,7 +3,7 @@ defmodule Utils.HttpClient do
     HTTPoison abstraction
   """
   def get(url) do
-    case HTTPoison.get(url) do 
+    case HTTPoison.get(url, [], [ssl: [{:versions, [:'tlsv1.2']}]]) do 
       {:ok, %HTTPoison.Response{status_code: status, body: body}} -> {:ok, status, body}
       {:error, %HTTPoison.Error{reason: reason}} -> {:error, reason}
     end
